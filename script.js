a59cb215a4c7d68f66204610c5d40e3a2b0ee1f2
christian kouyate <christiankouyate8@gmail.com>
2025-12-03 15:49:18 +0000

    ajout de script

---
+++ script.js
@@ -0,0 +1,5 @@
+function clic(){
+    document.getElementById("reac")
+    
+    .innerHTML="La vie ne nous fait pas de cadeaux donc au pétit matin tu as deux choix : soit tu te lève pour réaliser ton rêve 🦾🧠💲 soit tu te recouche puis tu continues ce rêve utopique 😴 🦋 "
+}
